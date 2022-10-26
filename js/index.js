a3fdcb5e98d44ed75e5de1e2a7c1aa7d6ec4dd42
Alex G. Jensen <jindoohex@hotmail.com>
2022-10-26 12:44:57 +0200

    Added event listeners for goods + hiding items

--- js/index.js
+++ js/index.js
@@ -1,2 +1,66 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+// document.getElementById("addGoodsButton").addEventListener('click',addGoods);
+// let elementInput = document.getElementById('addGoods');
+
+// function addGoods(e){
+//     console.log("In the addgoods function");
+//     let inputText = (elementInput).value;
+//     console.log("the input text is : " + inputText);
+
+//     let listUnhealthy = document.getElementById("listUnhealthy");
+//     let newLi = document.createElement('li');
+//     let newText = document.createTextNode(inputText)
+//     newLi.appendChild(newText);
+//     newLi.setAttribute('class','unhealthy');
+
+//     listUnhealthy.appendChild(newLi);
+// }
+
+
+// let elementsLi = document.getElementById('list').addEventListener('click', clickToHideItem)
+
+// function clickToHideItem(e)
+// {
+//     let clickElement = (e.target);
+    
+//     if(e.target && (clickElement).nodeName == "LI")
+//     {
+//         console.log(clickElement.id + " was clicked")
+//         clickElement.setAttribute('hidden', 'true');
+//     }
+// }
+
+// AddEventListener
+const getList = document.getElementById('list')
+const getButton = document.getElementById('addGoodsButton')
+
+// Adding goods button
+getButton.addEventListener('click', 'addGoods')
+function addGoods(e){
+    console.log("In the addgoods function");
+    let inputText = (elementInput).value;
+    console.log("the input text is : " + inputText);
+
+    let listUnhealthy = document.getElementById("listUnhealthy");
+    let newLi = document.createElement('li');
+    let newText = document.createTextNode(inputText)
+    newLi.appendChild(newText);
+    newLi.setAttribute('class','unhealthy');
+
+    listUnhealthy.appendChild(newLi);
+
+}
+
+// Hiding items on click
+getList.addEventListener('click', clickToHideItem)
+function clickToHideItem(e)
+{
+    let clickElement = (e.target);
+    
+    if(e.target && (clickElement).nodeName == "LI")
+    {
+        console.log(clickElement.id + " was clicked")
+        clickElement.setAttribute('hidden', 'true');
+    }
+}
